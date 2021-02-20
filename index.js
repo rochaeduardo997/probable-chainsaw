01cc1c50fbb270f7bb308ea3c60e51cae2855ca1
rochaeduardo997 <rochaeduardo997@protonmail.com>
2021-02-20 17:51:06 -0300

    Fixed bugs, start and finish date-time and a lot of another things

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 const childProcess = require('child_process');
+const fs = require('fs');
 const os = require('os');
 
 //GLOBAL
 var getHostname = os.hostname();
-var getDateTime = new Date().toISOString().slice(0, 10);
+var getDate = new Date().toISOString().slice(0, 10);
+var getHour = new Date().toString().slice(16, 24);
 
 async function main() {
-  mysqlBackup();
-  directoryBackup();
+  console.log('------------------------------------------------------------');
+  console.log('Backup function started at', getDate, getHour, '\n');
+
+  await Promise.all([
+    mysqlBackup(),
+    directoryBackup(),
+  ]);
+
+  let getDateFinishedAt = new Date().toISOString().slice(0, 10);
+  let getHourFinishedAt = new Date().toString().slice(16, 24);
+  console.log('\nBackup function finished at', getDateFinishedAt, getHourFinishedAt);
+  console.log('------------------------------------------------------------');
 }
 main();
 
 function mysqlBackup() {
   return new Promise((resolve, reject) => {
     try {
-      let databaseUsername = 'username-here';
-      let databasePassword = 'password-here';
-      let databaseName = 'database-here';
+      let databaseUsername = 'dbUsername';
+      let databasePassword = 'dbPassword';
+      let databaseName = 'dbName';
 
-      let databaseBackupFilename = './sql-bkp/sql-bkp-' + getHostname + '-' + getDateTime + '.sql.bkp';
+      let destinyDirectory = './sql-bkp/';
 
-      console.log('Starting database backup of', databaseName);
+      let databaseBackupFilename = destinyDirectory + 'sql-bkp-' + getHostname + '-' + getDate + '.sql.bkp';
 
-      childProcess.exec(`mysqldump --single-transaction -u${databaseUsername} -p${databasePassword} ${databaseName} > ${databaseBackupFilename}`, (error) => {
-          if (!error) {
-            resolve(console.log('\nDatabase backup stored at file:', databaseBackupFilename.split('./sql-bkp/').splice(1).toString()));
-          } else {
-            reject(console.error('\nFailed backup database: ', error));
-          }
-      });
+      if (fs.existsSync(destinyDirectory)) {
+        if (!fs.existsSync(databaseBackupFilename)) {
+
+          childProcess.exec(`mysqldump --single-transaction -u${databaseUsername} -p${databasePassword} ${databaseName} > ${databaseBackupFilename}`, (error) => {
+            if (!error) {
+              resolve(console.log('Database backup stored at file:', databaseBackupFilename.split('./sql-bkp/').splice(1).toString()));
+            } else {
+              childProcess.exec(`rm -f ${databaseBackupFilename}`)
+              resolve(console.error('Failed backup database: ', error));
+            }
+          });
+        } else {
+          resolve(console.error('Database - File already exists'));
+        }
+      } else {
+        resolve(console.error('nDatabase - Destiny directory doesn\'t exists'));
+      }
     } catch (error) {
-      reject(console.error('\nFailed backup database: ', error));
+      reject(console.error('Catch - Failed backup database: ', error));
     }
   });
 }
@@ -38,20 +60,27 @@ function mysqlBackup() {
 function directoryBackup() {
   return new Promise((resolve, reject) => {
     try {
-      let fromDirectoryName = './file-or-directory-here';
-      let toDirectoryName = ('./dir-bkp/dir-bkp-' + getHostname + '-' + getDateTime + '.tar.gz');
+      let fromDirectoryName = './folderOrFile';
+      let destinyDirectory = './dir-bkp/';
+      let toDirectoryName = (destinyDirectory + 'dir-bkp-' + getHostname + '-' + getDate + '.tar.gz');
 
-      console.log('\nStarting compression of', fromDirectoryName);
-
-      childProcess.exec(`tar -czf ${toDirectoryName} ${fromDirectoryName}`, (error) => {
-        if (!error) {
-          resolve(console.log('\nCompression completed, file generated:', toDirectoryName.split('./dir-bkp/').splice(1).toString()));
+      if (fs.existsSync(fromDirectoryName)) {
+        if (!fs.existsSync(toDirectoryName)) {
+          childProcess.exec(`tar -czf ${toDirectoryName} ${fromDirectoryName}`, (error) => {
+            if (!error) {
+              resolve(console.log('Compression completed, file generated:', toDirectoryName.split('./dir-bkp/').splice(1).toString()));
+            } else {
+              resolve(console.error('Failed to compress directory: ', error));
+            }
+          });
         } else {
-          reject(console.error('\nFailed to compress directory: ', error));
+          resolve(console.log('Directory - File already exists'));
         }
-      });
+      } else {
+        resolve(console.log('Directory or file doesn\'t exists'));
+      }
     } catch (error) {
-      reject(console.error('\nFailed to compress directory: ', error));
+      reject(console.error('Failed to compress directory: ', error));
     }
   });
 }

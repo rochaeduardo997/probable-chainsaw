64ffe6042174d7736c9f0f6f66c166b02809210a
rochaeduardo997 <rochaeduardo997@protonmail.com>
2021-02-21 11:18:52 -0300

    Fixed bug that don't showed variable content on log and added Host on database dump

--- index.js
+++ index.js
@@ -30,9 +30,10 @@ main();
 function mysqlBackup() {
   return new Promise((resolve, reject) => {
     try {
-      let databaseUsername = 'dbUsername';
-      let databasePassword = 'dbPassword';
-      let databaseName = 'dbName';
+      let databaseHost = 'localhost';
+      let databaseUsername = 'username';
+      let databasePassword = 'password';
+      let databaseName = 'database';
 
       let destinyDirectory = './sql-bkp/';
 
@@ -41,20 +42,20 @@ function mysqlBackup() {
       if (fs.existsSync(destinyDirectory)) {
         if (!fs.existsSync(databaseBackupFilename)) {
 
-          childProcess.exec(`mysqldump --single-transaction -u${databaseUsername} -p${databasePassword} ${databaseName} > ${databaseBackupFilename}`, (error) => {
+          childProcess.exec(`mysqldump --single-transaction -h${databaseHost} -u${databaseUsername} -p${databasePassword} ${databaseName} > ${databaseBackupFilename}`, (error) => {
             if (!error) {
-              resolve(console.log('Database backup stored at file:', databaseBackupFilename.split('./sql-bkp/').splice(1).toString()));
+              resolve(console.log(`Database backup stored at file: ${databaseBackupFilename.split('./sql-bkp/').splice(1).toString()}`));
             } else {
-              childProcess.exec(`rm -f ${databaseBackupFilename}`)
+              childProcess.exec(`rm -f ${databaseBackupFilename}`);
               // console.error(error);
-              resolve(console.log('Failed to backup database'));
+              resolve(console.log(`Database communication failed, \n  Host:${databaseHost} User:${databaseUsername} Password:${databasePassword} Database:${databaseName}`));
             }
           });
         } else {
-          resolve(console.error('Database - File already exists'));
+          resolve(console.log('Database - File already exists'));
         }
       } else {
-        resolve(console.error('nDatabase - Destiny directory doesn\'t exists'));
+        resolve(console.log('Database - Destiny directory doesn\'t exists'));
       }
     } catch (error) {
       reject(console.error('Catch - Failed backup database: ', error));
@@ -65,7 +66,7 @@ function mysqlBackup() {
 function directoryBackup() {
   return new Promise((resolve, reject) => {
     try {
-      let fromDirectoryName = './folderOrFile';
+      let fromDirectoryName = './fileOrDirectory';
       let destinyDirectory = './dir-bkp/';
       let toDirectoryName = (destinyDirectory + 'dir-bkp-' + getHostname + '-' + getDate + '.tar.gz');
 
@@ -73,7 +74,7 @@ function directoryBackup() {
         if (!fs.existsSync(toDirectoryName)) {
           childProcess.exec(`tar -czf ${toDirectoryName} ${fromDirectoryName}`, (error) => {
             if (!error) {
-              resolve(console.log('Compression completed, file generated:', toDirectoryName.split('./dir-bkp/').splice(1).toString()));
+              resolve(console.log(`Compression completed, file generated: ${toDirectoryName.split('./dir-bkp/').splice(1).toString()}`));
             } else {
               // console.error(error);
               resolve(console.log('Failed to compress directory'));
@@ -83,7 +84,7 @@ function directoryBackup() {
           resolve(console.log('Directory - File already exists'));
         }
       } else {
-        resolve(console.log('Directory or file doesn\'t exists'));
+        resolve(console.log(`Directory or file doesn\'t exists,\n  ${fromDirectoryName} cannot be found`));
       }
     } catch (error) {
       reject(console.error('Failed to compress directory: ', error));
